d998cb1353369276f11a9f45b777dbdb29b3f6ff
mdersuneli <muhammed.dersuneli@mycontrol.de>
2024-07-28 14:06:12 +0200

    selectLang init

---
+++ src/main/java/com/cab/SelectLanguage.java
@@ -0,0 +1,17 @@
+package com.cab;
+
+public class SelectLanguage {
+
+    public SelectLanguage(GamePanel gp) {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void draw() {
+        
+    }
+    
+}
