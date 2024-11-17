e21ccc8184e8fbb2f7fd65bbb010f83debcd5e6a
Memo PC <m.dersuneli@outlook.de>
2024-11-17 15:54:03 +0100

    bettler

--- src/main/java/com/cab/cardGame/EffekteMangaer.java
+++ src/main/java/com/cab/cardGame/EffekteMangaer.java
@@ -147,6 +147,7 @@ public class EffekteMangaer {
 			case  109: return new Witwe(card, cardGame, cardGame.boardState, triggerOnAddKreaturToGrave, ignoreState);
 			case  110: return new Scharmanin(card, cardGame, cardGame.boardState, triggerKarteWurdeDurchKampfZerstoert, ignoreState);
 			case  114: return new Donteur(card, cardGame, cardGame.boardState, triggerKreaturAufrufen, cardGame.effektSelectOponentBoardState);
+			case  116: return new Bettler(card, cardGame, cardGame.boardState, triggerOnWin, ignoreState);
 			case  121: return new Mumie(card, cardGame, cardGame.boardState, triggerKreaturAufrufen, ignoreState);
 			case  122: return new Koyote(card, cardGame, cardGame.boardState, triggerKarteHatDurchAngriffKarteZerstoert, cardGame.selectOptionCardListState);
 			case  123: return new Rabe(card, cardGame, cardGame.boardState, triggerKreaturAufrufen, ignoreState);
