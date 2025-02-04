0791d41adbb82d70235f2be584bb66d188d3fb5a
Memo PC <m.dersuneli@outlook.de>
2025-02-04 20:36:41 +0100

    firstStart

--- src/main/java/com/cab/states/FirstStart.java
+++ src/main/java/com/cab/states/FirstStart.java
@@ -10,8 +10,10 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.cab.GamePanel;
+import com.cab.Main;
 import com.cab.card.Art;
 import com.cab.card.Card;
+import com.cab.configs.Colors;
 
 public class FirstStart extends GameState {
     GamePanel gp;
@@ -80,11 +82,19 @@ public class FirstStart extends GameState {
     @Override
     public void draw(Graphics2D g2) {
         g2.drawImage(gp.imageLoader.genersichBG, 0, 0, gp.screenWidth, gp.screenHeight, null);
+        gp.drawLib.drawDialog(g2, gp.p(2), gp.p(3.5), gp.p(34), gp.p(16));
+
         g2.setFont(gp.font(36));
-        g2.setColor(Color.YELLOW);
+        g2.setColor(Colors.orangeYellow);
+        gp.drawLib.drawHover(g2, gp.p(1.5), gp.p(1.3), gp.p(39), gp.p(1), true);
         g2.drawString(gp.t("firstStateWillkommen"), gp.p(2), gp.p(2));
 
-        int cardSpacing = gp.p(2); 
+        g2.setColor(Color.RED);
+        gp.drawLib.drawHover(g2, gp.p(17.8), gp.p(17.4), gp.p(1.5), gp.p(1), true);
+        gp.drawLib.drawArrowOnState(g2, gp.p(16), gp.p(17), true, true);
+        g2.drawString("OK", gp.p(18), gp.p(18.15));
+
+        int cardSpacing = gp.p(3); 
 
         for (int i = 0; i < gp.player.truhe.size(); i++) {
             int abstandX = (i > 10) ? cardSpacing * (i - 10) : cardSpacing * i;
@@ -105,8 +115,12 @@ public class FirstStart extends GameState {
             if (y < yZiel) {
                 yPositions.set(i, y + 20);
             }
-        }
 
-        g2.drawString(gp.t("fWeiter"), gp.p(2), gp.p(19));
+            if (x >= xZiel && y >= yZiel) {
+                g2.setColor(Color.WHITE);
+                g2.setFont(gp.font(12));
+                g2.drawString(card.getName(), x, y + gp.p(3.3));
+            }
+        }
     }
 }
